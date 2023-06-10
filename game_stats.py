505cb01b19c25c374305643259fd6501ef2dd806
Tuan <anhtuan11b3@gmail.com>
2023-06-10 22:23:49 +0700

    Alien Invasion

---
+++ game_stats.py
@@ -0,0 +1,26 @@
+class GameStats:
+    """Track statistics for Alien Invasion."""
+
+    def __init__(self, ai_game):
+        """Initialize statistics."""
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+        # Start Alien Invasion in an active state.
+        self.game_active = False
+
+        # High score should never be reset.
+        self.filename = "HighScore.txt"
+        try:
+            with open(self.filename, "r") as f:
+                self.high_score = int(f.read())
+        except FileNotFoundError:
+            with open(self.filename, "w") as f:
+                f.write("0")
+                self.high_score = 0
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ship_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
